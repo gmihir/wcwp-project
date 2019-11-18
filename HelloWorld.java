3a09c64505e6b2af4ce7a9f70606aa77991d3880
Mihir Gupta <m08.in.us@gmail.com>
2019-11-18 00:12:51 -0800

    testing if this works with IntelliJ

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,13 @@
-class HelloWorld {
+public class HelloWorld {
 
-public static void main(String[] args) {
-	System.out.println("Hello World.");
+    public static void main(String[] args) {
+        Introduction(); // just testing some stuff
 }
+
+    public static void Introduction() {
+        System.out.println("You control the United Nations. The year is 1970, and scientists have begun to realize \n" +
+                "there is a possibility human atmospheric pollution has damaged the natural ecosystem.");
+    }
+
 
 }
