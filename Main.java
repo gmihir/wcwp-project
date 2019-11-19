45c87bc7cc6ef4f2e2d3e5309f9a2f94ca4f4037
Mihir Gupta <m08.in.us@gmail.com>
2019-11-19 14:24:49 -0800

    adding method for wrapping in star

--- Main.java
+++ Main.java
@@ -5,6 +5,19 @@ public class Main {
         Introduction(); // just testing some stuff
 }
 
+    public String wrapStars(String message) {
+        String ret = "";
+        for(int i = 1 ; i < 4 ; i++)
+        {
+            for(int j = 0 ; j < message.length() + 3 ; j++)
+            {
+                if(i%2 == 1)
+                    ret += "*";
+            }
+        }
+
+    }
+
     public static void Introduction() {
         System.out.println("Please enter your name.");
         Scanner scan = new Scanner(System.in);
@@ -13,6 +26,11 @@ public class Main {
 
         System.out.println("You control the United Nations. The year is 1970, and scientists have begun to realize \n" +
                 "there is a possibility human atmospheric pollution has damaged the natural ecosystem."); // i think i figured it out
+    }
+
+    public static void LevelOne() {
+
+
     }
 
 
@@ -27,6 +45,14 @@ public class Player {
         level = 0;
         this.name = name;
         System.out.println("Hello " + name + " ");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getLevel() {
+        return level;
     }
 
 }
